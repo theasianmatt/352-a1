e01f50607aa1deb7d5617cbfa6c8977c939f06d4
winterNebs <winterNebs@gmail.com>
2021-02-07 15:07:14 -0500

    Change to use random heuristic rather than greedy Code can sovle 4 queen at least

--- code/nqueens.py
+++ code/nqueens.py
@@ -1,18 +1,18 @@
 import random
 # create starting spots for queens
 # start w one queen per column, all in first row
-max_steps = 100000000000
+max_steps = 1000
 
 def print_board(queens, board_size):
     for i in range(board_size):
         print("X",end='')
     print()
     for row in range(board_size):
-        for column in range(board_size+1):
+        for column in range(board_size):
             occupied=False
             for i, q in enumerate(queens):
                 if i == column and q == row +1:
-                    print("Q",end='')
+                    print(validate(queens,i),end='')
                     occupied = True
 
             if not occupied:
@@ -21,9 +21,9 @@ def print_board(queens, board_size):
 
 def generate_start_board(board_size):
     start_positions = []
-    for j in range(board_size+1):
+    for j in range(board_size):
         # print(startPositions)
-        start_positions.append(j)
+        start_positions.append(random.randint(1,board_size))
         #print(start_positions)
         # Do something with list of starting positions
         #print(count_errors(start_positions))
@@ -36,7 +36,7 @@ def validate(queens, i):
 
     q = queens[i]
     for j, q2 in enumerate(queens):
-        if (i != j) and (i != 0) and (j!=0):
+        if i != j:
             # Same row (columns are always different)
             if q == q2:
                 errors += 1
@@ -51,11 +51,12 @@ def validate(queens, i):
 def count_errors(queens):
     errors = []
     total = 0
-    for i in range(1, len(queens)):
+    for i in range(len(queens)):
         e = validate(queens, i)
         total += e
         errors.append((i, e))
-    errors.sort(key=lambda tup: tup[1], reverse=True)
+    #errors.sort(key=lambda tup: tup[1], reverse=True)
+    random.shuffle(errors)
 #    print("Errors: ")
 #    print(errors)
     return (total, errors)
@@ -68,14 +69,12 @@ def solve(board_size):
     global max_steps
     # List of n queens
     queens = generate_start_board(board_size)
-#    print("Initial board:")
-#    print_board(queens,board_size)
     for count in range(max_steps):
         errors, error_queens = count_errors(queens)
         if errors > 0:
             # get the worst queen
-            #worst = error_queens.pop()
-            worst = error_queens[0]
+            worst = error_queens.pop()
+            #worst = error_queens[0]
             # do something
             # min conflict
             # take the queen with the most conflicts (first queen in error list)
@@ -88,18 +87,20 @@ def solve(board_size):
                 e = validate(tempq, worst[0])
                 if e < lowest:
                     lowest = e
-                    lowest_i = i
+                    lowest_i = i + 1
                 #print_board(tempq,board_size)
-                print("Loop {} errors: {} e: {}".format(count,errors,e))
-            print("Lowest {} lowest i {}".format(lowest,lowest_i))
-            print(error_queens)
-            queens[error_queens[0][0]] = lowest_i+1
-            print_board(queens,board_size)
+                #print("Loop {} errors: {} e: {}".format(count,errors,e))
+            #print("Lowest {} lowest i {}".format(lowest,lowest_i))
+            #print(error_queens)
+            queens[worst[0]] = lowest_i
+            #print_board(queens,board_size)
         else:
+            print("SOLVED in {}".format(count))
+            print(queens)
             return queens
 
     # should never get here unless fail
-    print("FAILED")
+    print("FAILED in {}".format(max_steps))
     return queens
 
 
